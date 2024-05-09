9f07f41cd233be1958dd6d4c70405689b954160c
CalotaTeodora <calotateodora74@gmail.com>
2024-05-09 12:04:10 +0300

    Created posts, delete posts, viewing user post. Hide login and sing-up for logged users.

--- website/models.py
+++ website/models.py
@@ -9,3 +9,26 @@ class User(db.Model, UserMixin):
     username = db.Column(db.String(150), unique=True)
     password = db.Column(db.String(150))
     date_created = db.Column(db.DateTime(timezone=True), default=func.now())
+    posts = db.relationship(
+        # reference all of this post that this user have
+        # and creates relationships between post and user
+        "Post",
+        backref="user",
+        passive_deletes=True,
+    )  # backref allows to write post.user then access the id of the user
+
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    text = db.Column(db.Text, nullable=False)
+    date_created = db.Column(db.DateTime(timezone=True), default=func.now())
+    author = db.Column(
+        db.Integer,
+        db.ForeignKey(
+            # take the id of the user that created this post
+            # if the user is deleted , the post will be deleted too
+            "user.id",
+            ondelete="CASCADE",
+        ),
+        nullable=False,
+    )
